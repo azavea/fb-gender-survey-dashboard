46f1e728a35231c6110e860c7641b502c8d6dc4e
Matthew McFarland <mmcfarland@azavea.com>
2020-12-01 12:13:17 -0500

    Demonstrate data fetched to application
    The temporary data is shown to be made available in the application.

--- src/app/src/App.js
+++ src/app/src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+    const [countries, setCountries] = useState({});
+
+    useEffect(() => {
+        // Temporary fetching of fake data
+        fetch(`${process.env.PUBLIC_URL}/data/country.json`)
+            .then(r => r.json())
+            .then(data => {
+                setCountries(data);
+            });
+    }, []);
+
     return (
         <div className='App'>
             <header className='App-header'>
-                <img src={logo} className='App-logo' alt='logo' />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className='App-link'
-                    href='https://reactjs.org'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                >
-                    Learn React
-                </a>
+                <h3>Gender Equality at Home</h3>
             </header>
+            <ul>
+                {countries.Label &&
+                    Object.entries(countries.Label).map(([id, label]) => {
+                        return <li key={id}>{label}</li>;
+                    })}
+            </ul>
         </div>
     );
 }
