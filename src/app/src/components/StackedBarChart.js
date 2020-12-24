9759a385e722245606cfcd2a298b0c7d1ff1c686
Tai Wilkin <twilkin@azavea.com>
2020-12-24 10:28:55 -0500

    Add Generalized Chart Layout
    Adds the basic layout for the generalized chart component and
    visualizations page. The generalized chart component handles the
    chart header and then passes the actual data rendering to the
    appropriate chart component. The chart components are stubbed out.

---
+++ src/app/src/components/StackedBarChart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box, Text } from '@chakra-ui/react';
+
+const StackedBarChart = ({ question, responses }) => {
+    return (
+        <Box>
+            <Text>Stacked Bar Chart</Text>
+        </Box>
+    );
+};
+
+export default StackedBarChart;
